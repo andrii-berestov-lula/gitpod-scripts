90462cac9af8f9fc6e60e1f9acdac3194e688bde
Andrew Berestov <andrew@berestov.dev>
2022-09-13 21:17:26 +0300

    should work now

--- install.fsx
+++ install.fsx
@@ -4,49 +4,65 @@ open System
 open System.IO
 open System.Net.Http
 open System.Runtime.InteropServices
+
 open type OperatingSystem
 
 open SharpCompress
 
-type OperatingSystem = Mac | Linux | Windows | Unsupported
-type CpuArchitecture = X64 | X86 | Arm64 | Unsupported
-type OsType = { Name: OperatingSystem; Architecture: CpuArchitecture }
+type OperatingSystem =
+    | Mac
+    | Linux
+    | Windows
+    | Unsupported
+
+type CpuArchitecture =
+    | X64
+    | X86
+    | Arm64
+    | Unsupported
+
+type OsType =
+    { Name: OperatingSystem
+      Architecture: CpuArchitecture }
 
 // TODO: Arm64 mac shows as X86 for some reason
-let osInfo: OsType  =
-    {
-        Name = if IsMacOS() then Mac elif IsWindows() then Windows elif IsLinux() then Linux else OperatingSystem.Unsupported
-        Architecture =
-            match Architecture() with
-            | Architecture.Arm64 -> Arm64
-            | Architecture.X86 -> X86
-            | Architecture.X64 -> X64
-            | _ -> Unsupported
-    }
+let osInfo: OsType =
+    { Name =
+        if IsMacOS() then Mac
+        elif IsWindows() then Windows
+        elif IsLinux() then Linux
+        else OperatingSystem.Unsupported
+      Architecture =
+        match Architecture() with
+        | Architecture.Arm64 -> Arm64
+        | Architecture.X86 -> X86
+        | Architecture.X64 -> X64
+        | _ -> Unsupported }
 
 let companionDownloadLink =
-    "https://gitpod.io/static/bin/gitpod-local-companion" +
-    match osInfo with
-    | {Name = Mac; Architecture = Arm64} -> "-darwin-arm64"
-    | {Name= Mac; Architecture=X86|X64}  -> "-darwin"
-    | {Name=Windows; Architecture=X64} -> "-windows.exe"
-    | {Name=Windows;Architecture=X86} -> "-windows-386.exe"
-    | {Name=Linux;Architecture=X64} -> "-linux-arm64"
-    | {Name=Linux; Architecture=X86} -> "-linux"
-    | {Name=_; Architecture=_} -> failwith "companion app is not supported for this platform"
+    "https://gitpod.io/static/bin/gitpod-local-companion"
+    + match osInfo with
+      | { Name = Mac; Architecture = Arm64 } -> "-darwin-arm64"
+      | { Name = Mac; Architecture = _ } -> "-darwin"
+      | { Name = Windows; Architecture = X64 } -> "-windows.exe"
+      | { Name = Windows; Architecture = X86 } -> "-windows-386.exe"
+      | { Name = Linux; Architecture = Arm64 } -> "-linux-arm64"
+      | { Name = Linux; Architecture = _ } -> "-linux"
+      | { Name = _; Architecture = _ } -> failwith "companion app is not supported for this platform"
 
-let oathkeeperDownloadLink = 
-    "https://github.com/ory/oathkeeper/releases/download/v0.39.4/" + 
-    match osInfo with
-    | {Name = Mac; Architecture = Arm64} -> "oathkeeper_0.39.4-macOS_arm64.tar.gz"
-    | {Name = Mac; Architecture = X64 | X86} -> "oathkeeper_0.39.4-macOS_64bit.tar.gz"
-    | {Name = Windows; Architecture = X64} -> "oathkeeper_0.39.4-windows_64bit.zip"
-    | {Name = Windows; Architecture = X86} -> "oathkeeper_0.39.4-windows_32bit.zip"
-    | {Name = Linux; Architecture = X64} -> "oathkeeper_0.39.4-linux_64bit.tar.gz"
-    | {Name = Linux; Architecture = X86} -> "oathkeeper_0.39.4-linux_32bit.tar.gz"
-    | {Name = _; Architecture = _} -> failwith "oathkeeper app is not supported for this platform"
+let oathkeeperDownloadLink =
+    "https://github.com/ory/oathkeeper/releases/download/v0.39.4/"
+    + match osInfo with
+      | { Name = Mac; Architecture = Arm64 } -> "oathkeeper_0.39.4-macOS_arm64.tar.gz"
+      | { Name = Mac; Architecture = _ } -> "oathkeeper_0.39.4-macOS_64bit.tar.gz"
+      | { Name = Windows; Architecture = X64 } -> "oathkeeper_0.39.4-windows_64bit.zip"
+      | { Name = Windows; Architecture = X86 } -> "oathkeeper_0.39.4-windows_32bit.zip"
+      | { Name = Linux; Architecture = X64|X86 } -> "oathkeeper_0.39.4-linux_64bit.tar.gz"
+      | { Name = Linux; Architecture = Arm64 } -> "oathkeeper_0.39.4-linux_arm64.tar.gz"
+      | { Name = Linux; Architecture = _} -> "oathkeeper_0.39.4-linux_32bit.tar.gz"
+      | { Name = _; Architecture = _ } -> failwith "oathkeeper app is not supported for this platform"
 
-let rec downloadTool (fullPath:string) =
+let rec downloadTool (fullPath: string) =
     let downloadFileTo (url: string) =
         task {
             use file = File.OpenWrite(fullPath)
@@ -55,44 +71,62 @@ let rec downloadTool (fullPath:string) =
             do! res.CopyToAsync(file)
             return fullPath
         }
-    
+
     match fullPath with
     | s when s.EndsWith "companion" -> downloadFileTo companionDownloadLink
-    | s when s.EndsWith "oathkeeper" -> downloadFileTo companionDownloadLink
+    | s when s.EndsWith "oathkeeper" -> downloadFileTo oathkeeperDownloadLink
     | _ -> failwith "No such tool"
-    
+
 let makeExecutable filepath =
     match osInfo.Name with
-    | Windows -> true
-    | _ -> 
+    | Windows -> ()
+    | _ ->
         let cmd = $"chmod +x {filepath}"
-        use proc = System.Diagnostics.Process.Start("/bin/bash", $"-c \"{cmd}\"")
-        proc.WaitForExit()
-        proc.ExitCode = 0
 
-(*let extract filepath =
-    GZipArchive
-    SharpCompress.Readers.Tar.TarReader.Open*)
+        use proc =
+            System.Diagnostics.Process.Start("/bin/bash", $"-c \"{cmd}\"")
+
+        proc.WaitForExit()
+        ()
+
+let extractInto toolsDir (filepath: string) =
+    let runCommand (cmd: string) (args: string) =
+        use proc = System.Diagnostics.Process.Start(cmd, args)
+        proc.WaitForExit()
+        ()
+        
+    match osInfo.Name with
+    | Windows -> runCommand "pwsh.exe" "--command \"Expand-Archive {filepath} {toolsDir}\""
+    | Linux | Mac ->
+        runCommand "/bin/bash" $"-c \"mv {filepath} {filepath}.tar\""
+        runCommand "/bin/bash" $"-c \"tar -xvf {filepath}.tar -C {toolsDir} {(filepath.Split '/') |> Array.last}\""
+        runCommand "/bin/bash" $"-c \"rm {filepath}.tar\""
+    | _ -> failwith "You're in a trouble"
 
 let printPass msg x =
     printf $"%s{msg}: %A{x}\n\n"
     x
- 
+
 // HOW IT SHOULD LOOK LIKE IN THE END
 let installTools toolsDir tools =
     printf $"Your OS info: %A{osInfo}\n"
-    if not (Directory.Exists toolsDir) then Directory.CreateDirectory toolsDir |> ignore
-    let fullPathTools = tools |> Seq.map ((+) toolsDir)
-        
-    fullPathTools
-        |> Seq.filter (File.Exists >> not) |> printPass "filtered"
-        |> Seq.map (fun f -> downloadTool f |> Async.AwaitTask |> Async.RunSynchronously) |> printPass "downloaded"
-        |> ignore
-    
-    fullPathTools
-        |> Seq.map makeExecutable |> printPass "made executable"
-        |> ignore
-        
 
-installTools "./bin/" [|"oathkeeper"; "companion"|] |> ignore
+    if not (Directory.Exists toolsDir) then
+        Directory.CreateDirectory toolsDir |> ignore
+
+    let fullPathTools =
+        tools |> Array.map ((+) toolsDir)
+
+    fullPathTools
+    |> Array.filter (File.Exists >> not)
+    |> Array.map (fun f ->
+        downloadTool f
+        |> Async.AwaitTask
+        |> Async.RunSynchronously)
+    |> Array.map (fun i -> 
+            if i.Contains("oathkeeper") then extractInto toolsDir i 
+            elif i.Contains("companion") then makeExecutable i          
+        )
     
+
+installTools "./bin/" [| "oathkeeper"; "companion" |]
